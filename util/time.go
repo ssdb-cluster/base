dcea1dbba17e1c27927d2cb3474a93900db9a8f2
ideawu <3202758+ideawu@users.noreply.github.com>
2021-08-03 23:24:52 +0800

    use monotonic time

--- util/time.go
+++ util/time.go
@@ -5,18 +5,24 @@ import (
 	"time"
 )
 
+var startTime time.Time = time.Now()
+
 func Sleep(second float64) {
 	time.Sleep((time.Duration)(second * 1000000) * time.Microsecond)
 }
 
+// monotonic time in seconds
 func Time() float64 {
-	return float64(time.Now().UnixNano()) / 1000 / 1000 / 1000;
+	return time.Since(startTime).Seconds()
+	// return float64(time.Now().UnixNano()) / 1000 / 1000 / 1000;
 }
 
 func Millitime() int64 {
-	return time.Now().UnixNano() / 1000 / 1000
+	return time.Since(startTime).Milliseconds()
+	// return time.Now().UnixNano() / 1000 / 1000
 }
 
 func Microtime() int64 {
-	return time.Now().UnixNano() / 1000
+	return time.Since(startTime).Microseconds()
+	// return time.Now().UnixNano() / 1000
 }
